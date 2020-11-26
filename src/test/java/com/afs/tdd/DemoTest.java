0bd571eaba1b5dc9980f97434f4a139120760847
JACKIE LEUNG (MIS-ISD-OOCLL/HKG) <LEUNGJA3@OOCL.COM>
2020-11-26 17:38:15 +0800

    [mars-rover][Jackie] implemented case when Move with direction E test

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -6,17 +6,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DemoTest {
     @Test
-    public void should_yPosition_plus_one_when_Move_given_direction_N_Position_0_0() {
-        int xPosition = 0;
-        int yPosition = 0;
+    public void should_return_yPosition_plus_one_when_Move_given_direction_N() {
+        int xPosition = 1;
+        int yPosition = 2;
         char direction = 'N';
 
         MarsRover marsRover = new MarsRover(xPosition,yPosition,direction);
         marsRover.Move();
 
-        assertEquals(0,marsRover.getXPosition());
-        assertEquals(1,marsRover.getYPosition());
+        assertEquals(1,marsRover.getXPosition());
+        assertEquals(3,marsRover.getYPosition());
         assertEquals('N',marsRover.getDirection());
+    }
+
+    @Test
+    public void should_return_xPosition_plus_one_when_Move_given_direction_E() {
+        int xPosition = 2;
+        int yPosition = 3;
+        char direction = 'E';
+
+        MarsRover marsRover = new MarsRover(xPosition,yPosition,direction);
+        marsRover.Move();
+
+        assertEquals(3,marsRover.getXPosition());
+        assertEquals(2,marsRover.getYPosition());
+        assertEquals('E',marsRover.getDirection());
     }
 
 }
