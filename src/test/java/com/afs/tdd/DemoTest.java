7c7bcf8e473c6b87a35c8f97ba85352f31438439
JACKIE LEUNG (MIS-ISD-OOCLL/HKG) <LEUNGJA3@OOCL.COM>
2020-11-26 17:48:27 +0800

    [mars-rover][Jackie] feat: implemented case when  MarsRover Move with direction S test

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -7,16 +7,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class DemoTest {
     @Test
     public void should_return_yPosition_plus_one_when_Move_given_direction_N() {
-        int xPosition = 1;
-        int yPosition = 2;
+        int xPosition = 2;
+        int yPosition = 3;
         String direction = "N";
 
         MarsRover marsRover = new MarsRover(xPosition,yPosition,direction);
         marsRover.Move();
 
-        assertEquals(1,marsRover.getXPosition());
-        assertEquals(3,marsRover.getYPosition());
-        assertEquals('N',marsRover.getDirection());
+        assertEquals(2,marsRover.getXPosition());
+        assertEquals(4,marsRover.getYPosition());
+        assertEquals("N",marsRover.getDirection());
     }
 
     @Test
@@ -30,7 +30,21 @@ class DemoTest {
 
         assertEquals(3,marsRover.getXPosition());
         assertEquals(2,marsRover.getYPosition());
-        assertEquals('E',marsRover.getDirection());
+        assertEquals("E",marsRover.getDirection());
+    }
+
+    @Test
+    public void should_return_yPosition_minus_one_when_Move_given_direction_S() {
+        int xPosition = 2;
+        int yPosition = 3;
+        String direction = "E";
+
+        MarsRover marsRover = new MarsRover(xPosition,yPosition,direction);
+        marsRover.Move();
+
+        assertEquals(2,marsRover.getXPosition());
+        assertEquals(2,marsRover.getYPosition());
+        assertEquals("S",marsRover.getDirection());
     }
 
 }
