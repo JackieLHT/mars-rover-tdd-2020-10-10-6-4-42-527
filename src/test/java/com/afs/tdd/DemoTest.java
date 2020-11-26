c052dfdcbeaa9392e817ae68e0658ea80319988c
JACKIE LEUNG (MIS-ISD-OOCLL/HKG) <LEUNGJA3@OOCL.COM>
2020-11-26 19:39:01 +0800

    [mars-rover][Jackier] feat: implemented case  MarsRover TurnRight with direction E test

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -131,4 +131,18 @@ class DemoTest {
         assertEquals("E",marsRover.getDirection());
     }
 
+    @Test
+    public void should_return_direction_S_when_TurnRight_with_direction_E() {
+        int xPosition = 2;
+        int yPosition = 3;
+        String direction = "E";
+
+        MarsRover marsRover = new MarsRover(xPosition,yPosition,direction);
+        marsRover.TurnRight();
+
+        assertEquals(2,marsRover.getXPosition());
+        assertEquals(3,marsRover.getYPosition());
+        assertEquals("S",marsRover.getDirection());
+    }
+
 }
